deeeded8a60db987a0cd715c07de9070dceeaccc
jorgealemangonzalez <jorge.alemangonzalez@gmail.com>
2018-09-03 20:42:30 +0100

    JSON parsing

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
-$( document ).ready(function() {
-    console.log( "ready!" );
-    var urlParams = new URLSearchParams(window.location.search);
-    console.log(urlParams);
+var DESCRIPTION_PARAM = "desc";
+var ERROR_FIELD_ID = "error-field";
+
+
+$(document).ready(function () {
+    try{
+        var urlParams = new URLSearchParams(window.location.search);
+        var desc = JSON.parse(urlParams.get(DESCRIPTION_PARAM));
+    }catch (e) {
+        document.getElementById(ERROR_FIELD_ID).innerText = "There was an error while parsing the description, it should be a JSON valid object.";
+    }
+
+    console.log();
+
 });
